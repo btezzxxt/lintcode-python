4b19f0e1661ad6ac7e982bbaf75966e0b1f96963
tongxi <xitongstone@hotmail.com>
2018-06-13 01:50:08 -0400

    quicksort start 461

---
+++ 461 kth smallest numbers in unsorted array.py
@@ -0,0 +1,10 @@
+# quick sort
+
+class Solution:
+    """
+    @param k: An integer
+    @param nums: An integer array
+    @return: kth smallest element
+    """
+    def kthSmallest(self, k, nums):
+        # write your code here

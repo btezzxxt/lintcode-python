ecb481a9c6562d8b5068a19fdef4fec17209c6ca
tongxi <xitongstone@hotmail.com>
2018-06-27 00:51:29 -0400

    update

--- 473 add and search word - data structure design.py
+++ 473 add and search word - data structure design.py
@@ -1,28 +1,27 @@
+# recursion, a much better version
+class TrieNode:
+    def __init__(self):
+        self.hasEnd = False
+        self.children = {}
+
+
 class WordDictionary:
     """
     @param: word: Adds a word into the data structure.
     @return: nothing
     """
     def __init__(self):
-        self.root = {
-            "children": {}
-        }
+        self.root = TrieNode()
         
 
     def addWord(self, word):
         # write your code here
         cur = self.root
-        length = len(word)
-        for i, letter in enumerate(word):
-            if not letter in cur["children"]:
-                cur["children"][letter] = { 
-                    "hasEnd": True if i == length - 1 else False,
-                    "children": {}
-                }
-            else:
-                if i == length - 1:
-                    cur["children"][letter]["hasEnd"] = True 
-            cur = cur["children"][letter]
+        for letter in word:
+            if not letter in cur.children:
+                cur.children[letter] = TrieNode()
+            cur = cur.children[letter]
+        cur.hasEnd = True
 
     """
     @param: word: A word could contain the dot character '.' to represent any one letter.
@@ -30,39 +29,30 @@ class WordDictionary:
     """
     def search(self, word):
         # write your code here
-        return self.searchInTrie(word, self.root)
+        return self.searchInTrie(word, 0, self.root)
 
-    def searchInTrie(self, word, trie):
-        cur = trie
+    def searchInTrie(self, word, index, cur):
         length = len(word)
-        for i, letter in enumerate(word):
+        if index == length:
+            return cur.hasEnd
+
+        for i in range(index, length):
+            letter = word[i]
             if letter == ".":
-                found = False
-                for child in cur["children"].keys():
-                    if i == length - 1:
-                        return True if cur["children"][child]["hasEnd"] else False
-                    else:
-                        found = found or self.searchInTrie(word[i+1:], cur["children"][child])
-                return found
-            else:
-                if not letter in cur["children"]:
-                    return False        
-                else:
-                    cur = cur["children"][letter]
-                    if i == length - 1 and cur["hasEnd"]:
+                for child in cur.children.keys():
+                    if self.searchInTrie(word, i+1, cur.children[child]):
                         return True
-        return False
+                return False
+            else:
+                if letter in cur.children:
+                    return self.searchInTrie(word, i+1, cur.children[letter])        
+                else:
+                    return False
 
 wd = WordDictionary()
-wd.addWord("ran")
-wd.addWord("rune")
-wd.addWord("runner")
-wd.addWord("runs")
-wd.addWord("add")
-wd.addWord("adds")
-wd.addWord("adder")
-wd.addWord("addee")
 
-print(wd.search("...s"))
+# wd.addWord("a")
+
+print(wd.search("."))
 
 

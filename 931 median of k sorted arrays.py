ba3616a6d083d64266ed3e972f291ddbc9cd8c97
txi <txi@factset.com>
2019-01-10 16:56:10 -0500

    push

--- 931 median of k sorted arrays.py
+++ 931 median of k sorted arrays.py
@@ -1,3 +1,4 @@
+import sys
 class Solution:
     """
     @param nums: the given k sorted arrays
@@ -5,4 +6,72 @@ class Solution:
     """
     def findMedian(self, nums):
         # write your code here
+        size = self.getTotalSize(nums)
+        if size == 0:
+            return 0.0
         
+        if size % 2 == 1:
+            return self.getKthInNums(nums, size // 2  + 1) * 1.0
+        else:
+            return (self.getKthInNums(nums, size // 2) + self.getKthInNums(nums, size // 2 +1)) / 2.0
+
+    def getKthInNums(self, nums, k):
+        l = -sys.maxsize
+        r = sys.maxsize
+        for arr in nums:
+            if not arr:
+                continue
+            l = min(l, arr[0])
+            r = max(r, arr[-1])
+
+        while l + 1 < r:
+            mid = (l + r) // 2
+            if self.numsLowerEqualToTarget(nums, mid) >= k:
+                r = mid
+            else:
+                l = mid
+        
+        if self.numsLowerEqualToTarget(nums, r) >= k:
+            return r
+            
+        if self.numsLowerEqualToTarget(nums, l) >= k:
+            return l
+
+        return 0 
+
+    def numsLowerEqualToTarget(self, nums, target):
+        total = 0
+        for arr in nums:
+            if not arr:
+                continue
+            
+            total += self.numsLowerEqualToTargetInArr(arr, target)
+        return total
+
+    def numsLowerEqualToTargetInArr(self, arr, target):
+        l = 0
+        r = len(arr) - 1
+        while l + 1 < r:
+            mid = (l + r) // 2
+            if arr[mid] > target:
+                r = mid
+            else:
+                l = mid
+        
+        if arr[r] <= target:
+            return r + 1
+        
+        if arr[l] <= target:
+            return l + 1
+        return 0
+
+    def getTotalSize(self, nums):
+        if not nums:
+            return 0
+        
+        total = 0
+        for arr in nums:
+            if not arr:
+                continue
+            total += len(arr)
+        return total

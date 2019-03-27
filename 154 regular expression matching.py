c8bda4264c68b440714c34100ffa05ae25ba41d6
txi <txi@factset.com>
2019-03-27 17:05:56 -0400

    regex match divide and conquor; 2 permutation tran

--- 154 regular expression matching.py
+++ 154 regular expression matching.py
@@ -1,4 +1,58 @@
-class Solution:
+# class Solution:
+#     """
+#     @param s: A string 
+#     @param p: A string includes "." and "*"
+#     @return: A boolean
+#     """
+#     def isMatch(self, s, p):
+#         # write your code here
+#         return self.dfs(s, p, 0, 0, {})
+    
+#     # devide and conquer + 记忆化搜索, return 从i， j开始匹配的字符串结果
+#     def dfs(self, s, p, i, j, memo):
+#         # 记忆化搜索不能忘了查找map
+#         if (i, j) in memo:
+#             return memo[(i, j)]
+
+#         if len(s) == i and len(p) == j:
+#             return True
+        
+#         if len(p) == j:
+#             return False
+        
+#         if len(s) == i:
+#             return self.is_empty(p[j:])
+
+#         l = s[i]
+#         r = p[j]
+
+#         res = False
+#         if j + 1 < len(p) and p[j + 1] == '*':
+#             # a* matches empty or a* matches more than one
+#             res = self.dfs(s, p, i, j + 2, memo) or (self.check_match(l, r) and self.dfs(s, p, i + 1, j, memo))      
+#         else:
+#             res = self.check_match(l, r) and self.dfs(s, p, i + 1, j + 1, memo)
+
+#         # 记下结果
+#         memo[(i, j)] = res
+#         return res
+
+#     def check_match(self, a, b):
+#         return a == b or b == '.'
+
+#     def is_empty(self, string):
+#         if len(string) % 2 != 0:
+#             return False
+
+#         for i in range(len(string)):
+#             if i % 2 == 1 and (string[i] != '*' or string[i-1] == '*'):
+#                 return False
+#         return True
+ 
+
+# print(Solution().isMatch("ab", ".*c"))
+
+class Solution2:
     """
     @param s: A string 
     @param p: A string includes "." and "*"
@@ -6,48 +60,48 @@ class Solution:
     """
     def isMatch(self, s, p):
         # write your code here
-        return self.dfs(s, p, 0, 0, {})
-    
-    # devide and conquer + 记忆化搜索, return 从i， j开始匹配的字符串结果
-    def dfs(self, s, p, i, j, memo):
-        # 记忆化搜索不能忘了查找map
-        if (i, j) in memo:
-            return memo[(i, j)]
+        return self.dfs(s, p, {})
 
-        if len(s) == i and len(p) == j:
+    def dfs(self, s, p, memo):
+        if (s, p) in memo:
+            return memo[(s, p)]
+        
+        if len(s) == 0 and len(p) == 0:
             return True
-        
-        if len(p) == j:
-            return False
-        
-        if len(s) == i:
-            return self.is_empty(p[j:])
-
-        l = s[i]
-        r = p[j]
-
-        res = False
-        if j + 1 < len(p) and p[j + 1] == '*':
-            # a* matches empty or a* matches more than one
-            res = self.dfs(s, p, i, j + 2, memo) or (self.check_match(l, r) and self.dfs(s, p, i + 1, j, memo))      
-        else:
-            res = self.check_match(l, r) and self.dfs(s, p, i + 1, j + 1, memo)
-
-        # 记下结果
-        memo[(i, j)] = res
-        return res
-
-    def check_match(self, a, b):
-        return a == b or b == '.'
-
-    def is_empty(self, string):
-        if len(string) % 2 != 0:
-            return False
-
-        for i in range(len(string)):
-            if i % 2 == 1 and (string[i] != '*' or string[i-1] == '*'):
+        elif len(s) == 0:
+            if len(p) % 2 == 1:
                 return False
-        return True
- 
-
+            for i, char in enumerate(p):
+                if i % 2 == 0 and p[i] == '*':
+                    return False
+                if i % 2 == 1 and p[i] != '*':
+                    return False
+            return True
+        elif len(p) == 0:
+            return False
+        
+        s_cur = s[0]
+        if len(p) == 1 or (len(p) > 1 and p[1] != '*'):
+            p_cur = p[0]
+            res = self.char_match(s_cur, p_cur) and self.dfs(s[1: ], p[1: ], memo)
+        else:
+            p_cur = p[0: 2]
+            res = self.dfs(s, p[2: ], memo) or (self.char_match(s_cur, p_cur) and self.dfs(s[1: ], p, memo))
+        
+        memo[(s, p)] = res 
+        return res
+    
+    def char_match(self, c1, c2):
+        if len(c2) == 1:
+            if c1 == c2 or c2 == '.':
+                return True
+            else:
+                return False
+        else:
+            if c1 == c2[0] or c2[0] == '.':
+                return True
+            else:
+                return False
 print(Solution().isMatch("ab", ".*c"))
+
+            
--- 425 letter combinations of a phone number.py
+++ 425 letter combinations of a phone number.py
@@ -1,3 +1,39 @@
+# class Solution:
+#     """
+#     @param digits: A digital string
+#     @return: all posible letter combinations
+#     """
+#     mapping = {
+#         "2": "abc",
+#         "3": "def",
+#         "4": "ghi",
+#         "5": "jkl",
+#         "6": "mno",
+#         "7": "pqrs",
+#         "8": "tuv",
+#         "9": "wxyz"
+#     }
+
+
+#     def letterCombinations(self, digits):
+#         # write your code here
+#         res = []
+#         self.dfs(digits, 0, [], res)
+#         return res 
+    
+#     def dfs(self, digits, i, path, res):
+#         if i == len(digits):
+#             if len(path) > 0:
+#                 res.append("".join(path))
+#             return 
+        
+#         digit = digits[i]
+#         letters = self.mapping[digit]
+#         for l in letters:
+#             path.append(l)
+#             self.dfs(digits, i + 1, path, res)
+#             path.pop()
+    
 class Solution:
     """
     @param digits: A digital string
@@ -14,23 +50,33 @@ class Solution:
         "9": "wxyz"
     }
 
-
     def letterCombinations(self, digits):
         # write your code here
-        res = []
-        self.dfs(digits, 0, [], res)
-        return res 
-    
-    def dfs(self, digits, i, path, res):
-        if i == len(digits):
-            if len(path) > 0:
-                res.append("".join(path))
-            return 
+        if digits == "":
+            return []
         
-        digit = digits[i]
-        letters = self.mapping[digit]
-        for l in letters:
-            path.append(l)
-            self.dfs(digits, i + 1, path, res)
-            path.pop()
-    
+        res = self.dfs(digits, {})
+        return res
+
+
+    def dfs(self, digits_left, memo):
+        if len(digits_left) == 0:
+            return ['']
+
+        if digits_left in memo:
+            return memo[digits_left]
+        
+        digit = digits_left[0]
+        
+        combo = self.dfs(digits_left[1: ], memo)
+        res = []
+        for char in self.mapping[digit]:
+            for string in combo:
+                res.append(char + string)
+        
+        memo[digits_left] = res
+        return res
+
+
+
+
